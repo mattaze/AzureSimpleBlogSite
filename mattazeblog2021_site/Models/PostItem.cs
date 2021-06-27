8ca4c0fcbc13f0b805915c8c5c886f4a5aca9e74
matt aze <aze.matt10@gmail.com>
2021-06-27 17:46:59 +0100

    Add project files.

---
+++ mattazeblog2021_site/Models/PostItem.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mattazeblog2021_site.Models
+{
+    public class PostItem : TableEntity
+    {
+        //Partition - project category
+        //RowKey - unique   date-name
+        public string Title { get; set; }
+        public string ImageURL { get; set; }
+        public string PostBody { get; set; }
+        public string Tags { get; set; }
+        public DateTime PostTime { get; set; }
+
+        public string getPostTimeUTC ()
+        {
+            return PostTime.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+        }
+    }
+}
---
+++ mattazeblog2021_site/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace mattazeblog2021_site.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public Models.PostItem LatestPost;
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            LatestPost = new Models.PostItem()
+            {
+                ImageURL = Url.Content("~/images/matrix-3109378_640.jpg"),
+                PostBody = "Her be some <strong>interesting</strong> text about the image or just rambling",
+                PostTime = DateTime.Now
+            };
+        }
+    }
+}
