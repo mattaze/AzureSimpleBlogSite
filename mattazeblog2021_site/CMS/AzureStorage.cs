b451963fa204db032935d53a65933fc3ce1b69c5
matt aze <aze.matt10@gmail.com>
2021-07-02 07:55:40 +0100

    remove double in index, template method added latest

---
+++ mattazeblog2021_site/CMS/AzureStorage.cs
@@ -0,0 +1,20 @@
+﻿using mattazeblog2021_site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mattazeblog2021_site.CMS {
+    public class AzureStorage {
+
+        public PostItem Get()
+        {
+            return null;
+        }
+
+        public PostItem Latest()
+        {
+            return null;
+        }
+    }
+}
